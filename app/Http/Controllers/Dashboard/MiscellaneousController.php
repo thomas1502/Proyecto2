b44feb69f42b38f038a451af19c6ab5ca004a15d
thomas1502 <thomas1502@mesoamericana.edu.gt>
2022-09-15 17:47:30 -0600

    Creación de Controladores P2

---
+++ app/Http/Controllers/Dashboard/MiscellaneousController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\Miscellany;
+use Illuminate\Http\Request;
+
+class MiscellaneousController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Miscellany  $miscellany
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Miscellany $miscellany)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Miscellany  $miscellany
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Miscellany $miscellany)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Miscellany  $miscellany
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Miscellany $miscellany)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Miscellany  $miscellany
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Miscellany $miscellany)
+    {
+        //
+    }
+}
